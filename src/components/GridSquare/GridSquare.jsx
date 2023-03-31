57f2876f1917ef97a79e985e340b5cca908b3e0d
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-03-30 18:37:44 -0700

    GridSquare component

---
+++ src/components/GridSquare/GridSquare.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const GridSquare = (props) => {
+  const classes = `grid-square color-${props.color}`;
+
+  return <div className={classes} />;
+};
+
+export default GridSquare;
