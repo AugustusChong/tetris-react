3e421ce770b6c754af665c44dbab28e30c1e686e
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-04-06 00:01:43 -0700

    Added grid area styles

--- src/components/ScoreBoard/ScoreBoard.jsx
+++ src/components/ScoreBoard/ScoreBoard.jsx
@@ -3,15 +3,19 @@ import "../../styles/score-board.css";
 
 export default function ScoreBoard(props) {
   return (
-    <div className="score-board">
-      <div>Score:{props.score}</div>
-      <div>Level: 1</div>
-      <button className="score-board-button" onClick={(e) => {}}>
-        Play
-      </button>
-      <button className="score-board-button" onClick={(e) => {}}>
-        Restart
-      </button>
-    </div>
+    <React.Fragment>
+      <div className="score-board">
+        <div>Score:{props.score}</div>
+        <div>Level: 1</div>
+      </div>
+      <div className="score-board-buttons">
+        <button className="score-board-button" onClick={(e) => {}}>
+          Play
+        </button>
+        <button className="score-board-button" onClick={(e) => {}}>
+          Restart
+        </button>
+      </div>
+    </React.Fragment>
   );
 }
