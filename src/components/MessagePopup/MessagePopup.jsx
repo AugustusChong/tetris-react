541d1a96588e7b2a927292b3e4fb3fbe78062ded
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-04-06 18:10:44 -0700

    Added initial popup message

---
+++ src/components/MessagePopup/MessagePopup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../styles/message.css";
+
+export default function MessagePopup(props) {
+  return (
+    <div className="message-popup">
+      <h1>Message Title</h1>
+      <p>Message Content</p>
+    </div>
+  );
+}
