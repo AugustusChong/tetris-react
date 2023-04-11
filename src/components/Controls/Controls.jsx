34407c61c219b65c4b6bc69f95b045ac809b9210
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-04-10 22:33:00 -0700

    Added connection for controls

--- src/components/Controls/Controls.jsx
+++ src/components/Controls/Controls.jsx
@@ -1,19 +1,47 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { moveLeft, moveRight, rotate, moveDown } from "../../actions";
 import "../../styles/controls.css";
 
 export default function Controls(props) {
+  const dispatch = useDispatch();
+  const isRunning = useSelector((state) => state.isRunning);
+
   return (
     <div className="controls">
-      <button className="control-button" onClick={(e) => {}}>
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveLeft());
+        }}
+      >
         Left
       </button>
-      <button className="control-button" onClick={(e) => {}}>
+
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveRight());
+        }}
+      >
         Right
       </button>
-      <button className="control-button" onClick={(e) => {}}>
+
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(rotate());
+        }}
+      >
         Rotate
       </button>
-      <button className="control-button" onClick={(e) => {}}>
+
+      <button
+        className="control-button"
+        onClick={(e) => {
+          dispatch(moveDown());
+        }}
+      >
         Down
       </button>
     </div>
