1253d0c52662f7e89f9931c8cd9de34287e751e5
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-04-10 22:58:32 -0700

    Added connection for message popup

--- src/components/MessagePopup/MessagePopup.jsx
+++ src/components/MessagePopup/MessagePopup.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "../../styles/message.css";
 
 export default function MessagePopup(props) {
+  const game = useSelector((state) => state.game);
+  const { isRunning, gameOver } = game;
+  let messageClass = "";
+  let message = "";
+
+  if (!isRunning && !gameOver) {
+    messageClass = "message-popup hidden";
+  } else if (!isRunning) {
+    messageClass = "message-popup";
+    message = "Paused";
+  } else if (gameOver) {
+    messageClass = "message-popup";
+    message = "Game Over";
+  }
+
   return (
-    <div className="message-popup">
-      <h1>Message Title</h1>
-      <p>Message Content</p>
+    <div className={messageClass}>
+      <h1>{message}</h1>
     </div>
   );
 }
