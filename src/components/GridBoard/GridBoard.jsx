d7acdd48bc613a020bc1530cb11178e8dc10ca51
Augustus Chong <112513956+AugustusChong@users.noreply.github.com>
2023-04-12 18:29:22 -0700

    Created timer

--- src/components/GridBoard/GridBoard.jsx
+++ src/components/GridBoard/GridBoard.jsx
@@ -1,14 +1,22 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import GridSquare from "../GridSquare/GridSquare";
+import { moveDown } from "../../actions";
 import { shapes } from "../../utils";
 
 export default function GridBoard(props) {
+  const requestRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+  const progressTimeRef = useRef(0);
+  const dispatch = useDispatch();
+
   const { grid, shape, rotation, x, y, isRunning, speed } = useSelector(
     (state) => state.game
   );
+
   const block = shapes[shape][rotation];
   const blockColor = shape;
+
   const gridSquares = grid.map((rowArray, row) => {
     return rowArray.map((square, col) => {
       const blockX = col - x;
@@ -29,6 +37,28 @@ export default function GridBoard(props) {
       return <GridSquare key={k} color={color} />;
     });
   });
+
+  const update = (time) => {
+    requestRef.current = requestAnimationFrame(update);
+    if (!isRunning) {
+      return;
+    }
+    if (!lastUpdateTimeRef.current) {
+      lastUpdateTimeRef.current = time;
+    }
+    const deltaTime = time - lastUpdateTimeRef.current;
+    progressTimeRef.current += deltaTime;
+    if (progressTimeRef.current > speed) {
+      dispatch(moveDown());
+      progressTimeRef.current = 0;
+    }
+    lastUpdateTimeRef.current = time;
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(update);
+    return () => cancelAnimationFrame(requestRef.current);
+  }, [isRunning]);
 
   return <div className="grid-board">{gridSquares}</div>;
 }
